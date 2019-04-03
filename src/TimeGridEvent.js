5c3fe6eba6e5610349ef330b7d6e226566ae0a50
Oliver Tang <olivertang@Olivers-MacBook-Pro.local>
2019-04-03 13:05:31 -0700

    Revamp calendar event look

--- src/TimeGridEvent.js
+++ src/TimeGridEvent.js
@@ -28,10 +28,10 @@ function TimeGridEvent(props) {
   let { height, top, width, xOffset } = style
   const inner = [
     <div key="1" className="rbc-event-label">
-      {label}
+      {Event ? <Event event={event} title={title} /> : title}
     </div>,
     <div key="2" className="rbc-event-content">
-      {Event ? <Event event={event} title={title} /> : title}
+      {label}
     </div>,
   ]
 
